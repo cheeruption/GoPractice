18df677ec8df7006e3e1a107038be9b993fa81ac
Viktor Snegirev <vssnegirev@MSK-C02CLR33ML7H.local>
2021-01-23 14:53:42 +0300

    epic2

--- src/github.com/cheeruption/firstapp/Main.go
+++ src/github.com/cheeruption/firstapp/Main.go
@@ -2,9 +2,32 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 )
 
+var i, j int = 1, 5
+
 func main() {
-	fmt.Println("hi there", rand.int(10))
+	k := "test_string"
+	fmt.Println("hi there", rand.Intn(99))
+	fmt.Println("This is pi", math.Pi)
+	fmt.Println(add(5, 6), add2(5, 6))
+	fmt.Println(swap("this", "that"))
+	fmt.Println(swap2("this", "that"))
+	fmt.Println("Объявления функции через var - ", i, j, "\n"+k)
+}
+func add(x int, y int) int {
+	return x + y
+}
+func add2(x, y int) int {
+	return x + y
+}
+func swap(x, y string) string {
+	return (y + " and " + x)
+}
+func swap2(x, y string) (y1, x1 string) {
+	y1 = "try"
+	x1 = "this"
+	return
 }
